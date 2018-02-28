646cf10bf07524c3ceb6f771348179349d1014f6
Felipe Pavan <pavan.fhw@gmail.com>
2018-02-28 11:49:01 -0300

    fix bug

--- src/providers/meeting/meeting.ts
+++ src/providers/meeting/meeting.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { API_ENDPOINT } from '../../models/consts';
 import { PeopleProvider } from '../../providers/people/people';
+import { Events } from 'ionic-angular/util/events';
 
 /*
   Generated class for the MeetingProvider provider.
@@ -12,10 +13,20 @@ import { PeopleProvider } from '../../providers/people/people';
 @Injectable()
 export class MeetingProvider {
 
-  constructor(public http: HttpClient, public peopleProvirder: PeopleProvider) {
+  currentUserMeetings;
+
+  constructor(public http: HttpClient, public peopleProvirder: PeopleProvider, public events: Events) {
     console.log('Hello MeetingProvider Provider');
+
+    events.subscribe('formigueiro de rua', (user) => {
+       http.post(API_ENDPOINT + '/searchMeetings', user.login).subscribe((data) => {
+        console.log(this.currentUserMeetings);
+        this.currentUserMeetings = data;
+      });
+    });
   }
 
+  
 
   addMeetig (meeting) {
     meeting.creator = this.peopleProvirder.currentUser.login;
