4fe976659f9153ceb8cd85c214ac2ec571ee6cd9
Guitakuwa <guilherme.takakuwa@gmail.com>
2018-03-01 10:27:20 -0300

    last adjustments

--- src/pages/user-profile/user-profile.ts
+++ src/pages/user-profile/user-profile.ts
@@ -22,6 +22,7 @@ export class UserProfilePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public peopleProvider: PeopleProvider, public events: Events, public meetingProvider: MeetingProvider) {
     this.friends = this.peopleProvider.currentUser.amigos;
+    this.initializeItems();
     events.subscribe('friend added', (data) => {
       this.friends = this.peopleProvider.currentUser.amigos;
       console.log(data.login);
@@ -30,7 +31,10 @@ export class UserProfilePage {
 
     
   }
-
+  searchUser() {
+    this.peopleProvider.searchFriends(this.person);
+    
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad UserProfilePage');
   }
@@ -39,11 +43,25 @@ export class UserProfilePage {
     login: ''
   }
 
-  searchUser() {
-    this.peopleProvider.searchFriends(this.person);
-    
+  
+  initializeItems() {
+    this.peopleProvider.currentUser.amigos; 
   }
 
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.peopleProvider.currentUser.amigos = this.peopleProvider.currentUser.amigos.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
   
 
 }
